c9dbcfb8e2b552593bde646ea55b2e0e21113432
Luke Walker <luke.walker@gmail.com>
2016-05-05 14:01:46 +0000

    Intial commit

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
 var express = require('express')
-var moment = require('moment')
-var fs = require('fs')
-var path = require('path')
-
-var app = express()
-
-port = process.env.PORT || 8000;
-
-app.get('/:url', function(req, res) {
-  var input = req.params.url;
-
-  if(/^\d{8}$/.test(input)) {
-    var parsed = moment(input, "X")
-  } else {
-    var parsed = moment(input, "MMMM D, YYYY")
-  }
-  if(parsed.isValid()) {
-    res.json({
-      unix: parsed.format("X"),
-      natural: parsed.format("MMMM D, YYYY")
-    });
-  } else {
-    res.json({
-      unix: null,
-      natural: null
-    })
-  }
-
-})
+var app = express();
+var parser = require('ua-parser-js');
 
 
-app.get('/', function(req, res) {
-  var file = path.join(__dirname, 'index.html');
-  res.sendFile(file, function(err) {
-    if (err) {
-      console.log(err)
-      res.status(err.status).end()
-    } else {
-      console.log('Sent:', file)
-    }
+var port = process.env.PORT || 8000;
+
+app.get('*', function(req, res) {
+  var ipaddress = req.headers['x-forwarded-for']
+  var lang  = req.headers['accept-language']
+  var ua = parser(req.headers['user-agent'])
+  var os = ua.os
+  
+  console.log(JSON.stringify(req.headers))
+  res.json({
+    ipaddress: ipaddress,
+    language: lang,
+    software: os
+
   })
 })
 
